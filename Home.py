1762f4c8e96a5a9b028629ee0f4dc3ba320edd9e
Baylen <baylenchalopa@gmail.com>
2025-03-13 23:17:56 +0300

    Made a few changes to make the site an easier read

--- Home.py
+++ Home.py
@@ -1,34 +1,46 @@
 import time
 import streamlit as st
 import pandas as pd
-@st.dialog("User Sign in")
-def user_sign_in():
-    users_name = st.text_input("Enter your user name : ")
-    users_email = st.text_input("Enter your email here : ")
-    return users_name, users_email
+
+
+def balloons_congrats():
+    st.balloons()
+
+@st.dialog("User Sign Up")
+def user_sign_up():
+    my_container = st.container()
+    with my_container:
+        user_name = st.text_input("Enter your user name :")
+        st.text_input("Enter your email here : ")
+        sign_up_button = st.button("Sign up")
+        if sign_up_button:
+            st.toast(f" {user_name} has been signed up successfully", icon="👍")
+            balloons_congrats()
+            my_container.empty()
+
+
+
 def main():
-    st.title("This is a title")
-    st.header("This is a header")
-    st.subheader("This is a subheader")
-    st.write("Welcome to my streamlit application built in Python")
-    st.caption("This is a caption")
+    st.title("Welcome to Baylens Streamlit app")
+    st.header("This is a Tutorial")
+    st.subheader("Doing a run through of the basics of Streamlit")
+    st.write("Mwah 😚")
+    st.caption("I hope this is easy on your eyes?")
     with st.sidebar:
         st.write("This is within the sidebar")
-    st.code("""
-    st.title("This is a title")
-    st.header("This is a header")
-    st.subheader("This is a subheader")
-    st.write("Welcome to my streamlit application built in Python")
-    st.caption("This is a caption")
-    a = 24
-    b = 19
-    st.write(f"{a} + {b} = {a + b}")
-    """)
 
-    with st.echo():
-        st.write("This code is going to be executed")
 
+
+    if st.button("Login"):
+        user = user_sign_up()
+
+    else:
+        pass
     st.divider()
+
+    terms_conditions = st.checkbox("I have read the terms and conditions and agree to them")
+    if terms_conditions:
+        st.write("User has agreed to the above terms and conditions")
     # st.write("------------------------------")
 
     # df = pd.read_csv("data.csv")
@@ -43,30 +55,13 @@ def main():
 
     st.metric("Rainfall in mm", 1400, -350)
 
-    if st.button("Submit"):
-        st.write("The button has been clicked")
 
-    like_dislike = st.feedback()
-    # st.write(like_dislike)
-    if like_dislike == 0:
-        st.write("You have disliked the content")
-    elif like_dislike == 1:
-        st.write("You have liked the content")
 
-    stars = st.feedback("stars")
-    if stars or stars == 0:
-        if stars <= 1:
-            st.write("Low Rating")
-        elif stars == 2:
-            st.write("Medium Rating")
-        elif stars > 2:
-            st.write("High Rating")
 
-        activated = st.toggle("Activate")
 
-    terms_conditions = st.checkbox("I have read the terms and conditions and agree to them")
-    if terms_conditions:
-        st.write("The user has agreed to the above terms and conditions")
+
+
+
 
     gender = st.radio("Select your Gender : ", ["Male", "Female", "Other"])
     if gender == "Male":
@@ -81,45 +76,89 @@ def main():
 
     st.write("Any changes made will be effected")
     st.select_slider("Enter your clothing size here : ", ["XS","S","L","XL","XXL","XXL"])
-    st.number_input("Enter a number here : ", 18,40,29)
-    st.date_input("Enter the date of your appointment here : ")
-    st.time_input("Enter the desired time of your appointment : ")
-    st.text_input("Enter your name here")
-    st.text_area("In less than 450 words, explain why you are deserving of the merit scholarship", 450)
-    st.file_uploader("Upload your file : ")
-    # st.camera_input("Take a photo of yourself")
+    st.number_input("Enter your age here : ", 18,40,29)
+    st.date_input("Enter the date of our anniversary here : ")
+    st.time_input("Enter the time  : ")
+    st.text_input("Enter my name here")
+    st.text_area("In less than 450 words....Im joking, go drink water, now",max_chars=450)
 
-    with st.expander(label="About"):
-        st.write("This is the first detail")
-        st.write("This is the second detail")
-        st.write("This is the third detail")
-        st.write("This is the fourth detail")
-    # with st.spinner("Loading your data..."):
-    #     time.sleep(2)
-    #     st.write("Your data has been loaded successfully")
-    #     st.balloons()
+    st.info("Upload a .csv file")
+    uploaded_file  = st.file_uploader("Upload your file : ")
+    if uploaded_file:
+        st.success("You have successfully uploaded a file")
+        st.image(uploaded_file)
+    else:
+        pass
+    st.header("Its Picture Time")
+    activated = st.toggle("Toggle to take a picture of yourself")
+    if activated:
+        st.camera_input("Take a photo of yourself")
+        balloons_congrats()
+        st.write("Thank you for your Picture, you Pretty Strawberry")
+    else:
+        pass
 
-    # for i in range(100):
-    #     st.progress(i)
-    #     time.sleep(1)
-    # progress_text = "Processing data"
-    # my_bar = st.progress(0, text=progress_text)
-    # for percentile_complete in range(100):
-    #     time.sleep(0.01)
-    #     my_bar.progress(percentile_complete +1 , text=progress_text)
-    # time.sleep(1)
-    # my_bar.empty()
+    with st.expander(label="About her"):
+        st.write("Strawberry")
+        st.write("Princess")
+        st.write("Darling")
+        st.write("Mommy")
+    with st.spinner("Loading your data..."):
+        time.sleep(1)
+        st.write("Your personal data has been loaded successfully 😈")
 
 
-    # user_sign_in()
-    st.toast("User singed up successfully", icon="👍")
-    st.write("You are doing well sir :hot_face: :pensive_face:")
+
+
+    progress_text = "Processing data"
+    my_bar = st.progress(0, text=progress_text)
+    for percentile_complete in range(100):
+        time.sleep(0.01)
+        my_bar.progress(percentile_complete +1 , text=progress_text)
+    time.sleep(1)
+    my_bar.empty()
+
+
+
+
+    st.write("You are doing well, Strawberry 🤗")
+    like_dislike = st.feedback()
+    # st.write(like_dislike)
+    if like_dislike == 0:
+        st.write("My baby doesnt like the page?:pleading_face:")
+    elif like_dislike == 1:
+        st.write("Thank you Darling ")
+
+    stars = st.feedback("stars")
+    if stars or stars == 0:
+        if stars <= 1:
+            st.write("Low Rating")
+        elif stars == 2:
+            st.write("Medium Rating")
+            st.write(":pleading_face:")
+        elif stars > 2:
+            st.write("High Rating")
+            balloons_congrats()
 
 
     # callout messages
-    st.success("You have successfully uploaded a file")
-    st.info("Upload a .csv file")
-    st.warning("The directory you are trying to access does not exist")
-    st.error("Failed to load the image")
+
+
+    st.warning(f"This is the end of my web app, Princess")
+    # st.error("Failed to load the image")
+    # st.code("""
+    #     st.title("This is a title")
+    #     st.header("This is a header")
+    #     st.subheader("This is a subheader")
+    #     st.write("Welcome to my streamlit application built in Python")
+    #     st.caption("This is a caption")
+    #     a = 24
+    #     b = 19
+    #     st.write(f"{a} + {b} = {a + b}")
+    #     """)
+
+    # with st.echo():
+    #     st.write("This code is going to be executed")
 if __name__ == "__main__":
     main()
+
---
+++ pages/Partners.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def main():
+    st.title('Partners')
+
+if __name__ == '__main__':
+    main()
